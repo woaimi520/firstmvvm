a57e53ce9d2deede4b0a2c3e5ee2465edc212818
woaimi520 <woaimi520@163.com>
2018-11-24 11:14:03 +0800

    2

--- app/src/main/java/com/renyu/administrator/myapplication/NewsModel.java
+++ app/src/main/java/com/renyu/administrator/myapplication/NewsModel.java
@@ -1,8 +1,16 @@
 package com.renyu.administrator.myapplication;
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * 新闻网络请求model
@@ -14,7 +22,7 @@ public class NewsModel {
     Reasult result;
     ArrayList<String> datalist = new ArrayList<String>();
     String dataUril="";
-
+    final static String TAG = "NewsModel";
 
     public NewsModel(NewslistBean bean,Reasult result) {
         this.bean = bean;
@@ -23,16 +31,47 @@ public class NewsModel {
 
     public void getData(){
 
-        double data = Math.random() * 9;
-       if(data>5) {
-           datalist.add(String.valueOf(data));
-           datalist.add(dataUril);
-         //  bean.setCtime(String.valueOf(data));
-           result.onSuccess(datalist);
-       }else{
-         //  bean.setCtime("fail");
-           result.onFail();
-       }
+        Observable.create(new Observable.OnSubscribe<Integer>() {
+
+
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                subscriber.onNext(9);
+
+
+            }
+        }).subscribe(new Subscriber<Integer>() {
+
+
+
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                Log.d(TAG, "对Next事件"+ integer +"作出响应"  );
+                double data = integer;
+                if(data>5) {
+                    datalist.add(String.valueOf(data));
+                    datalist.add(dataUril);
+                    //  bean.setCtime(String.valueOf(data));
+                    result.onSuccess(datalist);
+                }else{
+                    //  bean.setCtime("fail");
+                    result.onFail();
+                }
+            }
+        });
+
+
+
 
     }
 
